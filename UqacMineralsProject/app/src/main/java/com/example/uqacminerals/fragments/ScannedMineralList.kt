1f51eba670fa90b9a82bb7c5397f0ef35794034b
Nathan Rougier <nat.rougier38@gmail.com>
2022-03-22 19:45:34 -0400

    ajout page description minérau

--- UqacMineralsProject/app/src/main/java/com/example/uqacminerals/fragments/ScannedMineralList.kt
+++ UqacMineralsProject/app/src/main/java/com/example/uqacminerals/fragments/ScannedMineralList.kt
@@ -5,17 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.example.uqacminerals.R
 
 
 class ScannedMineralList : Fragment() {
 
+    val minerals = arrayOf(
+        "Mineral 1", "Mineral 2",
+        "Mineral 3", "Mineral 4",
+        "Mineral 5", "Mineral 6",
+        "Mineral 7", "Mineral 8",
+        "Mineral 9"
+    )
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scanned_mineral_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_wiki, container, false)
+        val wikiListView : ListView = view.findViewById(R.id.listViewMinerals)
+        val arr: ArrayAdapter<String> = ArrayAdapter(
+            view.context,
+            R.layout.listview_item,
+            R.id.listViewItem_mineralName,
+            minerals
+        )
+        wikiListView.adapter = arr
+
+        // Inflate the layout for this fragment
+        return view
     }
 
 }
