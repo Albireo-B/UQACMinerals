77c7fff0509c5b5488eafcf4c1588b4a131f60a5
Nico <nicey74300@gmail.com>
2022-03-22 22:13:50 -0400

    First version of qr scanner

--- UqacMineralsProject/app/src/main/java/com/example/uqacminerals/ui/main/MainActivity.kt
+++ UqacMineralsProject/app/src/main/java/com/example/uqacminerals/ui/main/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.uqacminerals.ui.main
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager.widget.ViewPager
 import com.example.uqacminerals.R
 import com.example.uqacminerals.classes.ViewPagerAdapter
+import com.tbruyelle.rxpermissions2.RxPermissions
+
 
 class MainActivity : FragmentActivity() {
 
     private lateinit var appViewPager : ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val rxPermissions = RxPermissions(this)
+        rxPermissions
+            .request(Manifest.permission.CAMERA) // ask single or multiple permission once
+            .subscribe { granted: Boolean ->
+                if (granted) {
+                    // All requested permissions are granted
+                } else {
+                    // At least one permission is denied
+                }
+            }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -18,5 +31,6 @@ class MainActivity : FragmentActivity() {
 
         appViewPager.adapter = ViewPagerAdapter(supportFragmentManager)
     }
+
 
 }
