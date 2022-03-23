c39e0c56567dca9ede41321428c64f907e0d5b79
Nico <nicey74300@gmail.com>
2022-03-23 11:37:08 -0400

    Corrected qr scanner

--- UqacMineralsProject/app/src/main/java/com/example/uqacminerals/ui/main/MainActivity.kt
+++ UqacMineralsProject/app/src/main/java/com/example/uqacminerals/ui/main/MainActivity.kt
@@ -1,29 +1,37 @@
 package com.example.uqacminerals.ui.main
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager.widget.ViewPager
 import com.example.uqacminerals.R
 import com.example.uqacminerals.classes.ViewPagerAdapter
-import com.tbruyelle.rxpermissions2.RxPermissions
 
 
 class MainActivity : FragmentActivity() {
 
     private lateinit var appViewPager : ViewPager
-
+    private val PERMISSION_REQUEST_CODE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
-        val rxPermissions = RxPermissions(this)
-        rxPermissions
-            .request(Manifest.permission.CAMERA) // ask single or multiple permission once
-            .subscribe { granted: Boolean ->
-                if (granted) {
-                    // All requested permissions are granted
-                } else {
-                    // At least one permission is denied
-                }
-            }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+
+
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.CAMERA),
+                123);
+
+            /*ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    PERMISSION_REQUEST_CODE);*/
+        }
+        else {
+
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
