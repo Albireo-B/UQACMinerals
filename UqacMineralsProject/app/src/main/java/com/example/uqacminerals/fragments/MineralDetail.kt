7387e44ec8ef9dbb8a5f44f4585b46ea7336c9d1
M5Altaïr <guillaume.vinet.38@gmail.com>
2022-04-26 19:21:23 -0400

    Fix images size and mineral image

--- UqacMineralsProject/app/src/main/java/com/example/uqacminerals/fragments/MineralDetail.kt
+++ UqacMineralsProject/app/src/main/java/com/example/uqacminerals/fragments/MineralDetail.kt
@@ -1,5 +1,7 @@
 package com.example.uqacminerals.fragments
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +14,10 @@ import androidx.fragment.app.setFragmentResultListener
 import com.example.uqacminerals.R
 import com.example.uqacminerals.database.MineralModel
 import com.google.gson.Gson
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 class MineralDetail : Fragment() {
@@ -30,15 +36,29 @@ class MineralDetail : Fragment() {
             val nameView: TextView = requireView().findViewById(R.id.mineralDetail_Nom) as TextView
             val descriptionView = requireView().findViewById(R.id.mineralDetail_Description) as TextView
             val imageView = requireView().findViewById(R.id.mineralDetail_Image) as ImageView
-
+            imageView.setImageBitmap(getBitmapFromURL(mineral.GetImage()))
             nameView.text = mineral.GetNom()
             descriptionView.text = mineral.GetDescription()
-            //imageView.setImageURI(mineral.GetImage())
 
         }
 
 
         return inflater.inflate(R.layout.fragment_mineral_detail, container, false)
+    }
+
+    fun getBitmapFromURL(src: String?): Bitmap? {
+        return try {
+            val url = URL(src)
+            val connection: HttpURLConnection = url
+                .openConnection() as HttpURLConnection
+            connection.setDoInput(true)
+            connection.connect()
+            val input: InputStream = connection.getInputStream()
+            BitmapFactory.decodeStream(input)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
     }
 
 }
